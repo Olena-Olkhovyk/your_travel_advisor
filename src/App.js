a79adffa62709d54f7f49c59fcb57bd8af3d379f
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-05-03 15:00:19 +0200

    Include components to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
-
+import { CssBaseline, Grid } from "@mui/material";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import { Css } from "@mui/icons-material";
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <CssBaseline>
+        <Header />
+        <Grid container spacing={3} style={{ width: "100%" }}>
+          <Grid item xs={12} md={4}>
+            <List />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Map />
+          </Grid>
+        </Grid>
+      </CssBaseline>
+    </>
+  );
 }
 
 export default App;
