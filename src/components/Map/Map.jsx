b7031b3bb240d14a6fb87411a6ac11188d5aec13
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-05-05 11:39:04 +0200

    Display map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import LocationOnOutlinedIcon from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
+import { ClassNames } from "@emotion/react";
 const Map = () => {
-  return <div>Map</div>;
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <div style={{ height: "85vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAJ7C99K3haDlwbJE6gm3-lNUb6sQzz1Oo" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
